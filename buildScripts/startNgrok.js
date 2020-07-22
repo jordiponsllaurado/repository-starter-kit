c0456e9253580f4e7a99da7a20dafbcbb97399b9
Jordi Pons <JPons@ohpen.com>
2020-07-22 15:52:23 +0200

    Improves share app link messages

--- buildScripts/startNgrok.js
+++ buildScripts/startNgrok.js
@@ -4,6 +4,8 @@ var chalk = require("chalk");
 (async function () {
   const url = await ngrok.connect(3000);
   const apiUrl = ngrok.getUrl();
-  console.log(chalk.green(`Nrog started, check ${apiUrl}\n`));
+
+  console.log(chalk.green(`Shareable link: ${url}`));
+  console.log(chalk.gray(`Nrog started, check ${apiUrl}\n`));
   console.log("Press Cntrl + ^C to stop the tunner");
 })();
